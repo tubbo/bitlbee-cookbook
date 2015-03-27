e92f9971b050a86a866314f0f5bf8e32a44ac6d4
Tom Scott <tscott@weblinc.com>
2015-03-27 16:33:58 -0400

    Add in missing attrs

--- providers/install.rb
+++ providers/install.rb
@@ -1,22 +1,40 @@
 action :install do
-  protocol_string = if @new_resource.protocols.any?
-    @new_resource.protocols.join("\s")
-  else
-    nil
-  end
 
   if @new_resource.from_package
-    include_recipe 'bitlbee::package'
+    package 'bitlbee'
   else
-    include_recipe 'bitlbee::source'
+    source_location = File.join Chef::Config[:cache_path], 'bitlbee'
+
+    service 'bitlbee' do
+      action :enable
+    end
+
+    git source_location do
+      repository @new_resource.repository
+      branch @new_resource.branch
+    end
+
+    execute 'apt-get build-dep -y bitlbee' do
+      only_if { @new_resource.install_dependencies }
+    end
+
+    execute './configure && make && make install' do
+      cwd source_location
+      notifies :restart, 'service[bitlbee]'
+    end
+  end
+
+  protocol_string = if @new_resource.protocols.any?
+    @new_resource.protocols.join("\s")
   end
 
   template '/etc/init/bitlbee.conf' do
     source 'bitlbee.conf.erb'
-    variables \
+    variables(
       user: @new_resource.user,
       port: @new_resource.port,
       protocols: protocol_string
+    )
 
     notifies 'service[bitlbee]', :restart
     action :create
--- resources/install.rb
+++ resources/install.rb
@@ -5,3 +5,4 @@ attribute :port,          kind_of: Fixnum,  default: 6667
 attribute :protocols,     kind_of: Array,   default: []
 attribute :from,          kind_of: String,  default: 'package'
 attribute :branch,        kind_of: String,  default: 'master'
+attribute :repository,    kind_of: String,  default: 'https://github.com/bitlbee/bitlbee.git'
