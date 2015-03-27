bf3a34a6c8b53f0c8b33657889dce163fb0c4413
Tom Scott <tscott@weblinc.com>
2015-03-27 15:52:50 -0400

    Fix attrs

--- resources/install.rb
+++ resources/install.rb
@@ -1,6 +1,7 @@
-actions :install, :remove
+actions :install, default: true
 
-attribute :user,          kind_of: String,                  default: 'bitlbee'
-attribute :port,          kind_of: Fixnum,                  default: 6667
-attribute :protocols,     kind_of: Array,                   default: []
-attribute :from_package,  kind_of: [TrueClass, FalseClass], default: false
+attribute :user,          kind_of: String,  default: 'bitlbee'
+attribute :port,          kind_of: Fixnum,  default: 6667
+attribute :protocols,     kind_of: Array,   default: []
+attribute :from,          kind_of: String,  default: 'package'
+attribute :branch,        kind_of: String,  default: 'master'
