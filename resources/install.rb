105c35cd8e369942c14f71c62674fd9194fcf7ef
Tom Scott <tscott@weblinc.com>
2015-03-27 14:56:03 -0400

    Line up arguments and fix arg for kind

--- resources/install.rb
+++ resources/install.rb
@@ -1,6 +1,6 @@
 actions :install, :remove
 
-attribute :user, kind: String, default: 'bitlbee'
-attribute :port, kind: Fixnum, default: 6667
-attribute :protocols, kind: Array, default: []
-attribute :from_package, kind: Boolean, default: false
+attribute :user,          kind_of: String,                  default: 'bitlbee'
+attribute :port,          kind_of: Fixnum,                  default: 6667
+attribute :protocols,     kind_of: Array,                   default: []
+attribute :from_package,  kind_of: [TrueClass, FalseClass], default: false
